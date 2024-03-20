2f0697271705606e8a5c832fd2811ff377a36bdf
Wolf-Hermit1993 <126912152+Wolf-Hermit1993@users.noreply.github.com>
2024-03-20 21:52:34 +0300

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -1,25 +1,27 @@
 <?php
-
+// Получаем данные из формы
 $name = $_POST['name'];
 $phone = $_POST['phone'];
-$massage = $_POST['message'];
-$token = "6662725518:AAGlfErzXCTU7flpWfo7xiXjSqWm4D4OEY0";
-$chat_id = "-1002112155507";
-$arr = array(
-  'Имя пользователя: ' => $name,
-  'Телефон: ' => $phone,
-  'Сообщение' => $message
-);
 
-foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
-};
+// Ваш API-ключ Telegram бота
+$telegram_api_key = '6662725518:AAGlfErzXCTU7flpWfo7xiXjSqWm4D4OEY0';
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+// Чат-идентификатор группы в Telegram, куда будут отправляться уведомления
+$chat_id = '-1002112155507';
 
-if ($sendToTelegram) {
-  header('Location: success.html');
+// Сообщение для отправки в группу
+$message = "Новая заявка на обратный звонок:\nИмя: $name\nНомер телефона: $phone";
+
+// URL для отправки сообщения в Telegram
+$telegram_url = "https://api.telegram.org/bot$telegram_api_key/sendMessage?chat_id=$chat_id&text=" . urlencode($message);
+
+// Отправляем запрос к API Telegram
+$response = file_get_contents($telegram_url);
+
+// Проверяем результат отправки
+if ($response === false) {
+    echo 'Ошибка при отправке заявки.';
 } else {
-  echo "Error";
+    echo 'Заявка успешно отправлена.';
 }
 ?>
